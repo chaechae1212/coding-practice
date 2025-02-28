d51b927d74f700de89cfe6522fe8ba1b6a957c7a
chae <81283827+chaechae1212@users.noreply.github.com>
2025-02-28 13:00:53 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -0,0 +1,7 @@
+def solution(strlist):
+    small_list=[]
+    for i in range(0,len(strlist)):
+        mylist=strlist[i]
+        num=len(mylist)
+        small_list.append(num)
+    return small_list
