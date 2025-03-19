934aee8c6e34d167eeb5e333a63e5d840e23d78f
chae <81283827+chaechae1212@users.noreply.github.com>
2025-03-19 11:20:22 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int chicken) {
         int coupon=chicken;
-        int remember=0;
+        int service_chicken=0;
+        
         while(coupon>=10){
-      
-        int service_chicken=coupon/10;
-        remember+=service_chicken;
-        coupon=service_chicken+(coupon%10);
+        service_chicken+=coupon/10;
+        coupon=coupon/10+(coupon%10);
         
     }
-        return remember;
+        return service_chicken;
   }
 }
