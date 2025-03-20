bb0dbe9b099930bd996e971c06557bc079458413
chae <81283827+chaechae1212@users.noreply.github.com>
2025-03-20 12:21:31 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/README.md
+++ 프로그래머스/0/120846. 합성수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 합성수 찾기 - 120846 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.22 ms
+메모리: 89.9 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 11:08:24
+2025년 03월 20일 12:21:29
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int count;
+        int result=0;
+        for(int j=1;j<=n;j++){
+            count=0;
+            for(int i=1;i<=j;i++){
+                if(j%i==0){
+                    count++;
+                
+            }}
+            if(count>=3){
+                result+=1;
+            }
+       
+                
+            }
+        return result;
+        }
+        
+        }
+
