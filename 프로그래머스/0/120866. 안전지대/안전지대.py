2da01263598776268effb6b3ac2ad08d4c685185
chae <81283827+chaechae1212@users.noreply.github.com>
2025-03-21 17:16:41 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,50 @@
+def solution(board):
+    point=[]
+    box=[]
+    j=0
+    n=len(board[0])
+    for b in board:
+        for i in range(0,n):
+            for value in [b[i]]:
+                if value==1:
+                    point=[j,i]
+                    box.append(point)
+            
+        j+=1
+        
+    for i in range(len(box)):
+        row,col=box[i]
+        board[row][col]=2
+     
+        if col-1>=0:
+            board[row][col-1]=2
+            
+        if col+1<n:
+            board[row][col+1]=2
+            
+        if row-1>=0:
+            board[row-1][col]=2
+            
+        if row+1<n:
+            board[row+1][col]=2
+            
+        if row-1>=0 and col-1>=0:
+            board[row-1][col-1]=2
+            
+        if row-1>=0 and col+1<n:
+            board[row-1][col+1]=2
+            
+        if row+1<n and col-1>=0:
+            board[row+1][col-1]=2
+            
+        if row+1<n and col+1<n:
+            board[row+1][col+1]=2
+        
+    total=0
+    for row in board:
+            total+=row.count(2)
+    return n*n-total
+     
+    
+                
+                
