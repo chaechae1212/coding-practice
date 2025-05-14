ffb35d2d9f24679422ae9949f2aa57f5e387e225
chae <81283827+chaechae1212@users.noreply.github.com>
2025-05-14 11:45:05 +0900

    [level 0] Title: 팩토리얼, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
@@ -1,15 +1,4 @@
 function solution(n) {
-    let sum=1;
-    for(let i=1;i<=n;i++){
-        sum=1;
-        for(let j=1;j<=i;j++){
-            sum*=j;
-            if (sum==n){
-                return i;
-            }
-            if(sum>n){
-                return i-1;
-            }
-        }
-    }
+    var answer = 0;
+    return answer;
 }
